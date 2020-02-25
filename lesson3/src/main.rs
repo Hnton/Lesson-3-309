ca236c96da8037d0c561cc5d97d24debfb2e2e5d
Mikael Hinton <43019867+Hnton@users.noreply.github.com>
2020-02-25 15:49:31 -0500

    Resources & max

--- lesson3/src/main.rs
+++ lesson3/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let mut f = BufReader::new(File::open("input1.txt").unwrap());
+    let mut f = BufReader::new(File::open("input2.txt").unwrap());
 
     let mut s = String::new();
     f.read_line(&mut s).unwrap();
@@ -14,13 +14,13 @@ fn main() {
         .collect();
 
     let array_clone = arr.clone();
-    println!("{:?}", array_clone);
+    println!("Input:     {:?}", array_clone);
 
-    let resource = array_clone[0].len();
+    let resource_number = array_clone[0].len();
     //RESOURCE CLONE
-    let _resource_clone = resource.clone();
+    let _resource_number_clone = resource_number.clone();
 
-    println!("Resources: {}", _resource_clone);
+    println!("Resources: {}", _resource_number_clone);
 
     let processes = (array_clone.len() - 1)/2;
     //PROCESS CLONE
@@ -32,7 +32,24 @@ fn main() {
     // AVAILABLE CLONE
     let available_clone = available.clone();
 
-    println!("Available: {:?}", available_clone);
+    println!("Available:  {:?}", available_clone);
+
+    let _resources: Vec<Vec<i32>> = array_clone[1..processes_clone+1].iter().cloned().collect();
+
+    // RESOURCE CLONE
+    let _resources_clone = _resources.clone();
+
+    println!("Resources: {:?}", _resources_clone);
+
+    let _max: Vec<Vec<i32>> = array_clone[processes_clone+1..array_clone.len()].iter().cloned().collect();
+
+    //MAX CLONE
+    let _max_clone = _max.clone();
+
+    println!("Max:       {:?}", _max);
+
+
+
 
 
 
